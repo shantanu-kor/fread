6b340367a6bc0d89136588aad546a8b75b5886fb
shantanu-kor <kor.shantanu1@gmail.com>
2025-01-06 12:51:17 +0530

    made changes

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="exnd-curses",
-    version="0.1.4",
-    description="Simple use of curses.",
+    name="fread",
+    version="0.1.0",
+    description="Used exnd-curses to output file content.",
     author="Shantanu Kor",
     author_email="kor.shantanu1@gmail.com",
-    url="https://github.com/shantanu-kor/exnd-curses.git",
+    url="https://github.com/shantanu-kor/fread.git",
     packages=find_packages(),
-    install_requires=[],
-    extras_require={
-	"windows": ["windows-curses"],
-	"linux": ["curses"],
-    },
+    install_requires=["exnd-curses"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
